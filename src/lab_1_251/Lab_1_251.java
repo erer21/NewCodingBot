6f53db5aed62190dea3cdb52f3561c57c9e0fd0d
erada <erada.a@yahoo.com>
2023-03-18 03:45:28 +0300

    First commit!

---
+++ src/lab_1_251/Lab_1_251.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab_1_251;
+
+/**
+ *
+ * @author erada
+ */
+public class Lab_1_251 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
